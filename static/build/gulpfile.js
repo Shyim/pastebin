d5fd75f34d113475e5db819d487d064989d133dc
Shyim <shyim@outlook.de>
2015-09-19 20:28:57 +0200

    Fixed Codemirror PHP Mode

--- static/build/gulpfile.js
+++ static/build/gulpfile.js
@@ -3,17 +3,18 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
-gulp.task('scss', function () {
+gulp.task('default', function () {
 	gulp.src('pastebin/theme.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('../css'));
 });
 
-gulp.task('js', function() {
+gulp.task('default', function() {
 	gulp.src([
 		'bower_components/jquery/dist/jquery.js',
 		'bower_components/codemirror/lib/codemirror.js',
 		'bower_components/codemirror/mode/css/css.js',
+		'bower_components/codemirror/mode/clike/clike.js',
 		'bower_components/codemirror/mode/php/php.js',
 		'bower_components/codemirror/mode/javascript/javascript.js',
 		'pastebin/custom.js'
